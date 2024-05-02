81c435c4e2f553808a40a57567e170697749c0a2
kanimarat <kanykeimarat1@gmail.com>
2024-05-02 11:39:06 -0500

    added SignIn tests

---
+++ src/test/java/pages/signInPage.java
@@ -0,0 +1,8 @@
+package pages;
+
+public class signInPage {
+
+    public void signInTest1(){
+        System.out.println("Sign in");
+    }
+}
---
+++ src/test/java/tests/signInTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+public class signInTest {
+
+    @Test
+    public void signInTest2(){
+        System.out.println("Sign in page");
+    }
+}
